97aabf0918a4bfb13925d69f08be28e9dcb05e26
Sarbeswar Palai <121221696+Sabyapalai@users.noreply.github.com>
2025-01-04 20:17:29 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,22 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-//	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-//    private static final String USER = "system"; 
-//    private static final String PASSWORD = "1011";
     
     private static final String URL="jdbc:oracle:thin:@localhost:1521:xe";
     private static final String USER="system";
     private static final String PASSWORD="1011";
-    
-//    public static Connection getConnection() {
-//        try {
-//            return DriverManager.getConnection(URL, USER, PASSWORD);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException("Failed to connect to the database!");
-//        }
-//    }
     
     public static Connection getConnection() {
     	try {
